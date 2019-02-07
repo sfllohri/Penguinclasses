fb096eac27942c71b62595ab7826e1dbf5b3894e
Sebastian Lopez <sfllohri@gmail.com>
2019-02-07 11:48:24 -0300

    cambio

--- persona.py
+++ persona.py
@@ -1,17 +1,17 @@
 #Crear el archivo persona.py y el archivo dino.py dentro de la carpeta clases
-#Crear una clase persona() que tenga como atributos nombre, edad y profesión. Al instanciar la clase tiene que saludar igual 
+#Crear una clase persona() que tenga como atributos nombre, edad y profesión. Al instanciar la clase tiene que saludar igual
 #que el dino, diciendo sus atributos.
 
 class Persona:
     def __init__ (self, un_nombre, una_edad, una_prof):
-        self.nombre = un_nombre 
+        self.nombre = un_nombre
         self.edad = una_edad
         self.profesion = una_prof
         print ("Hola soy", self.nombre, "tengo", self.edad, "anhos", "y soy una", self.profesion)
-    
+
     def gritar(self):
         print("ESTOY GRITANDO!", self.profesion)
-    
+
     def cumple(self):
         self.edad=self.edad+1
         print('hoy es mi cumpleanhos y tengo:', self.edad)
@@ -22,6 +22,4 @@ print(charlie.nombre)  #aca se imprime solo el nombre de la clase, porque ya le 
 
 charlie.gritar()
 charlie.cumple()
-
-
-
+#esto es un cambio
